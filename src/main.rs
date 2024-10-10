283549a7c9d21a07d0b29a17ccc94bf59670bba6
Lucas Dutra Pereira <lucas.dutra7162@gmail.com>
2024-10-10 13:06:36 -0300

    Updated Guessing Game

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
-use std::io;
+extern  crate rand;
+
+use  std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input yout guess: ");
+    let secret_num = rand::thread_rng().gen_range(1..101);
+    println!("The secret number is: {}", secret_num);
+
+    loop {
+        println!("Please input yout guess: ");
     
-    let mut guess = String::new();
+        let mut guess = String::new();
+    
+        io::stdin().read_line(&mut guess).
+        expect("Failed to read line.");
+    
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number: ");
+    
+        println!("You guessed: {}", guess);
+    
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("YOU WIN!"),
+        }   
+    }
 }
