b419c01a109cf905c2953345674670df3e79df22
Lucas Dutra Pereira <lucas.dutra7162@gmail.com>
2024-10-11 12:17:18 -0300

    Removed: Secret number print.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ fn main() {
     println!("Guess the number!");
 
     let secret_num = rand::thread_rng().gen_range(1..101);
-    println!("The secret number is: {}", secret_num);
 
     loop {
         println!("Please input yout guess: ");
